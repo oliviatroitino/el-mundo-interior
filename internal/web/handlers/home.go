4689e413fd1521fc2d526f17c4fe4a8dc84428b7
oliviatroitino <olivia.troitino@live.u-tad.com>
2026-04-14 11:34:21 +0200

    Añadir función Home en home.go para usar el template con los mundos ordenados.

--- internal/web/handlers/home.go
+++ internal/web/handlers/home.go
@@ -1,1 +1,13 @@
 package handlers
+
+import (
+	"el-mundo-interior/internal/content"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	data := HomePageData{
+		Worlds: content.OrderedWorlds(),
+	}
+	render(w, "templates/pages/home.tmpl", data)
+}
